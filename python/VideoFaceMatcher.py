faff6dc00525a9acd00d66503145865d072166bc
Dmitry Studynskyi <dms@smartling.com>
2018-12-28 21:18:45 -0500

    Converted single quote to double quote

--- python/VideoFaceMatcher.py
+++ python/VideoFaceMatcher.py
@@ -16,7 +16,7 @@ from MatchedFace import MatchedFace
 
 
 class VideoFaceMatcher:
-    IMAGES_DIR = './'
+    IMAGES_DIR = "./"
     VALIDATED_IMAGES_MASK = "validated_images/*/*.jpg"
 
     GRAPH_FILENAME = "facenet_celeb_ncs.graph"
@@ -31,7 +31,7 @@ class VideoFaceMatcher:
     # the same face will return 0.0
     # different faces return higher numbers
     # this is NOT between 0.0 and 1.0
-    FACE_MATCH_THRESHOLD = 0.8
+    FACE_MATCH_THRESHOLD = 0.4
 
     # Print to console from static methods by default
     send_to_node = print_to_console
@@ -48,7 +48,7 @@ class VideoFaceMatcher:
             ts = time.time()
             result = method(*args, **kw)
             te = time.time()
-            VideoFaceMatcher.send_to_node('log', '%r  %2.2f ms' % (method.__name__, (te - ts) * 1000))
+            VideoFaceMatcher.send_to_node("log", "%r  %2.2f ms" % (method.__name__, (te - ts) * 1000))
             return result
 
         return timed
@@ -160,7 +160,7 @@ class VideoFaceMatcher:
     # @timeit
     def face_match(face1_output: numpy.ndarray, face2_output: numpy.ndarray) -> float:
         if len(face1_output) != len(face2_output):
-            VideoFaceMatcher.send_to_node('log', 'length mismatch in face_match. {} against {}'
+            VideoFaceMatcher.send_to_node("log", "length mismatch in face_match. {} against {}"
                                           .format(len(face1_output), len(face2_output)))
             return 100
         total_diff = 0
@@ -194,7 +194,7 @@ class VideoFaceMatcher:
             VideoFaceMatcher.send_to_node("log", "PASS!  Matched faces: {}".format(matched_faces))
 
         else:
-            VideoFaceMatcher.send_to_node("log", 'FAIL!  File does not match any image.')
+            VideoFaceMatcher.send_to_node("log", "FAIL!  File does not match any image.")
 
         return matched_faces
 
@@ -214,11 +214,11 @@ class VideoFaceMatcher:
 
         actual_camera_width = camera_device.get(cv2.CAP_PROP_FRAME_WIDTH)
         actual_camera_height = camera_device.get(cv2.CAP_PROP_FRAME_HEIGHT)
-        VideoFaceMatcher.send_to_node('log', 'actual camera resolution: {} x {}'
+        VideoFaceMatcher.send_to_node("log", "actual camera resolution: {} x {}"
                                       .format(actual_camera_width, actual_camera_height))
 
         if (camera_device is None) or (not camera_device.isOpened()):
-            VideoFaceMatcher.send_to_node('log', '''Could not open camera.  Make sure it is plugged in.
+            VideoFaceMatcher.send_to_node("log", '''Could not open camera.  Make sure it is plugged in.
             Also, if you installed python opencv via pip or pip3 you
             need to uninstall it and install from source with -D WITH_V4L=ON
             Use the provided script: install-opencv-from_source.sh''')
@@ -228,7 +228,7 @@ class VideoFaceMatcher:
             # Read image from camera,
             ret_val, vid_image = camera_device.read()
             if not ret_val:
-                VideoFaceMatcher.send_to_node('log', "No image from camera, exiting")
+                VideoFaceMatcher.send_to_node("log", "No image from camera, exiting")
                 self.stop()
                 break
 
@@ -254,7 +254,7 @@ class VideoFaceMatcher:
         # we need at least one
         devices = mvnc.EnumerateDevices()
         if len(devices) == 0:
-            VideoFaceMatcher.send_to_node('log', 'No NCS devices found')
+            VideoFaceMatcher.send_to_node("log", "No NCS devices found")
             quit()
 
         # Pick the first stick to run the network
@@ -266,11 +266,11 @@ class VideoFaceMatcher:
         # The graph file that was created with the ncsdk compiler
         graph_file_name = os.path.abspath(VideoFaceMatcher.GRAPH_FILENAME)
         if not os.path.isfile(graph_file_name):
-            VideoFaceMatcher.send_to_node('log', 'Cannot find graph file "{}"'.format(graph_file_name))
+            VideoFaceMatcher.send_to_node("log", 'Cannot find graph file "{}"'.format(graph_file_name))
             return
 
         # read in the graph file to memory buffer
-        with open(graph_file_name, mode='rb') as f:
+        with open(graph_file_name, mode="rb") as f:
             graph_in_memory = f.read()
 
         # create the NCAPI graph instance from the memory buffer containing the graph file.
@@ -285,9 +285,9 @@ class VideoFaceMatcher:
                 self.run_camera(validated_image_list, graph)
             else:
                 input_image_filename_list = os.listdir(VideoFaceMatcher.IMAGES_DIR)
-                input_image_filename_list = [i for i in input_image_filename_list if i.endswith('.jpg')]
+                input_image_filename_list = [i for i in input_image_filename_list if i.endswith(".jpg")]
                 if len(input_image_filename_list) < 1:
-                    VideoFaceMatcher.send_to_node('log', 'No .jpg files found')
+                    VideoFaceMatcher.send_to_node("log", "No .jpg files found")
                     return 1
                 # self.run_images(valid_output, self.validated_image_list, graph, input_image_filename_list)
         finally:
